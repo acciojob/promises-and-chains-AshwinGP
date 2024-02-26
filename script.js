eff04a740e2b63ae6ea2d7765ddeb3b177ac463f
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-26 12:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your JS code here. If required.
-let button=documnet.getElementById("")
+let button=documnet.getElementById("btn");
+var name=document.getElementById("name").value;
+var age=document.getElemenById("age").value;
+
+function  onClick(){
+	return promise=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18){
+				resolve(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				reject(`Welcome, ${name}. You can vote.`)
+			}
+		},4000)
+	})
+}
+button.addEventListener('click',onClick().then((res)=>{
+	alert(res);
+}))
